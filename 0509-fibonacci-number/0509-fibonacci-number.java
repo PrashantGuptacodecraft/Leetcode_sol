3b309e4d144797dbf611020b19cdb64104de6557
Prashant Gupta <adityagupta983869@gmail.com>
2026-09-03 23:48:10 +0530

    Time: 0 ms (100%), Space: 42 MB (52.55%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,17 @@
+class Solution {
+  
+    public int fib(int n){
+        if(n<=1) return n;
+            int[] dp=new int[3];
+            dp[0]=0;
+            dp[1]=1;
+          for(int i=1;i<=n-1;i++){
+            dp[2]=dp[0]+dp[1];
+            dp[0]=dp[1];
+            dp[1]=dp[2];
+          }
+          return dp[2];
+        }
+      
+    
+}
